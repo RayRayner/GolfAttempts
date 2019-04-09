4acfba10ada92cb342bcef042a4b3d3fb99920d2
Rayner <robrayner96@hotmail.com>
2019-04-09 20:06:29 +0100

    Materials and beginning of programming

---
+++ GolfAttempts/Assets/Scripts/BallShoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+
+public class BallShoot:MonoBehaviour {
+
+	public GameObject golfBall;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () 
+	{
+        rb = golfBall.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1)) 
+		{
+            rb.AddForceAtPosition(Vector3.forward*2, Vector3.back, ForceMode.Impulse);
+        }
+		if (Input.GetKeyDown(KeyCode.Alpha2)) 
+		{
+            rb.AddForceAtPosition(Vector3.forward*6, Vector3.back, ForceMode.Impulse);
+        }
+		if (Input.GetKeyDown(KeyCode.Alpha3)) 
+		{
+            rb.AddForceAtPosition(Vector3.forward*18, Vector3.back, ForceMode.Impulse);
+        }
+		if (Input.GetKeyDown(KeyCode.Alpha4)) 
+		{
+            rb.AddForceAtPosition(Vector3.forward*54, Vector3.back, ForceMode.Impulse);
+        }
+	}
+}
